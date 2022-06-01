8854efbd8527d83ad72409f7acf3283c4a77d420
AndBenvds404 <andres_benavides17@hotmail.com>
2022-05-31 19:53:59 -0600

    taller ejemplo esteoritpos Service y repositorio

---
+++ src/main/java/com/uce/edu/demo/repository/EstudianteRepositoryImpl.java
@@ -0,0 +1,37 @@
+package com.uce.edu.demo.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.uce.edu.demo.modelo.Estudiante;
+@Repository
+
+public class EstudianteRepositoryImpl implements IEstudianteRepository {
+
+	@Override
+	public void insertar(Estudiante e) {
+		//aqui se realiza la insercion en la base de datos
+		System.out.println("se ha insertado en la base el estudiante: "+ e);
+		
+	}
+
+	@Override
+	public Estudiante buscar(String apellido) {
+		System.out.println("se ha encontrado en la base el estudiante: "+ apellido);
+		return null;
+	}
+
+	@Override
+	public void actualizar(Estudiante e) {
+		System.out.println("se ha actualizado en la base el estudiante: "+ e);
+		
+	}
+
+	@Override
+	public void eliminar(String cedula) {
+		System.out.println("se ha eliminado en la base el estudiante: "+ cedula);
+		
+	}
+	
+	
+
+}
